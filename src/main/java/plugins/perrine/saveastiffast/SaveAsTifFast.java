ca07297fd5e9e3dc6c1c67738b2e626500c41d5f
PerrineGilloteaux <perrine.paul-gilloteaux@univ-nantes.fr>
2021-02-09 16:58:47 +0100

    convert to maven

--- src/main/java/plugins/perrine/saveastiffast/SaveAsTifFast.java
+++ src/main/java/plugins/perrine/saveastiffast/SaveAsTifFast.java
@@ -3,29 +3,48 @@ package plugins.perrine.saveastiffast;
 import plugins.adufour.ezplug.EzVarFile;
 import plugins.adufour.ezplug.EzVarSequence;
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
+import javax.swing.SwingUtilities;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import icy.file.Loader;
 import icy.gui.frame.progress.AnnounceFrame;
+import icy.gui.viewer.Viewer;
 import icy.imagej.ImageJUtil;
+import icy.main.Icy;
+import icy.plugin.PluginLauncher;
+import icy.plugin.PluginLoader;
 import plugins.adufour.blocks.lang.Block;
 import plugins.adufour.blocks.util.VarList;
+import plugins.adufour.ezplug.EzLabel;
 import plugins.adufour.ezplug.EzPlug;
 import plugins.adufour.ezplug.EzVar;
 import icy.sequence.Sequence;
-
+import icy.sequence.SequenceUtil;
 import ij.IJ;
 import ij.ImagePlus;
 
 public class SaveAsTifFast extends EzPlug implements Block {
 	private EzVar<Sequence> inputseq=new EzVarSequence("Sequence");
-	private EzVarFile outputfile=new EzVarFile("Output file", null);
+	private EzVarFile outputfile=new EzVarFile("Output file", inputseq.getValue().getFilename());
 	
 	@Override
 	protected void initialize() {
+		addEzComponent(new EzLabel(getVersionString()));
 	    inputseq.setToolTipText("Sequence to Save");
 	    outputfile.setToolTipText("Where to save as tif");
 		
@@ -83,9 +102,52 @@ public class SaveAsTifFast extends EzPlug implements Block {
     } 
     return true; 
 }
+	public static void main( final String[] args ) throws InvocationTargetException, InterruptedException
+	{
+		// Launch the application.
+		Icy.main( args );
 
+		// Load an image.
+		final String imagePath = "samples/Cont1.lsm";
+		final Sequence sequence = Loader.loadSequence( imagePath, 0, true );
+
+		
+
+		// Display the images.
+		SwingUtilities.invokeAndWait( () -> {
+			new Viewer( sequence );
+			
+		} );
+
+		// Run the plugin on the last active image (the copy).
+		PluginLauncher.start( PluginLoader.getPlugin( SaveAsTifFast.class.getName() ) );
+	}
 	@Override
 	public void clean() {
 		// 
+	}
+	
+	private String getVersionString(){
+		String version="unknown";
+		MavenXpp3Reader reader = new MavenXpp3Reader();
+		Model model;
+	   
+	      try {
+			model = reader.read(new FileReader("pom.xml"));
+			 version=model.getArtifactId()+model.getVersion();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	     
+	    
+		return version;
+	
 	}
 }
